e285d4113865ee0e87d0d65bdf2ca596043b21c1
{vongocchinh} <{ngocchinh1410@gmail.com}>
2021-01-20 00:43:30 +0700

    push first

---
+++ src/App.js
@@ -0,0 +1,69 @@
+
+function App() {
+  return (
+    <>
+    <div className="container">
+        <div className="container-flex">
+          <div className="container-left">
+            <dvi className="container-left-top">
+              <div className="images-cv">
+                <img alt="###" src="https://static.thenounproject.com/png/17241-200.png" />
+              </div>
+            </dvi>
+            <div className="container-left-bottom">
+              <div className="images-cv">
+                <h2>Hello</h2>
+                <h3>My name is Chinh</h3>
+              </div>
+            </div>
+          </div>
+          <div className="container-right">
+            <div className="container-right-top">
+              <div className="container-right-top-introducing">
+                <p>Introducing Your Self</p>
+                <p>Introducing With Text Boxes</p>
+                <div className="span-text">
+                  <span className="span1" />
+                  <span className="span2" />
+                  <span className="span3" />
+                </div>
+              </div>
+            </div>
+            <div className="container-right-bottom">
+              <div className="container-text-layout-down">
+                <div className="container-text">
+                  <div className="container-layout-text">
+                    <h4>Text Here 01</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
+                </div>
+                <div className="container-text">
+                  <div className="container-layout-text">
+                    <h4>Text Here 01</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="container-text-layout-up">
+                <div className="container-text">
+                  <div className="container-layout-text">
+                    <h4>Text Here 01</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
+                </div>
+                <div className="container-text">
+                  <div className="container-layout-text">
+                    <h4>Text Here 01</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
